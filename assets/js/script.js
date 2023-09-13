9400407ff378993ce8ec260478a6e432028b83c3
Jesus Rojas <jesusnicotra@gmail.com>
2023-09-13 20:15:07 -0300

    Cambios en la grafica y errores de carga

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,93 +1,120 @@
-async function obtenerDatosDiaActual() {
-    const res = await fetch('https://mindicador.cl/api');
-    const data = await res.json();
+let miGrafico = null;
 
-    return data;
+async function obtenerDatosDiaActual() {
+   try {
+      const res = await fetch("https://mindicador.cl/api");
+
+      if (!res.ok) {
+         throw new Error("No se pudo obtener los datos");
+      }
+
+      const data = await res.json();
+
+      return data;
+   } catch (error) {
+      const errorDiv = document.querySelector("#error");
+      errorDiv.textContent = `Error: ${error.message}`;
+   }
 }
 
 async function obtenerDatosHistoricos(moneda, fecha) {
-    const res = await fetch(`https://mindicador.cl/api/${moneda}/${fecha}`);
-    const data = await res.json();
+   try {
+      const res = await fetch(`https://mindicador.cl/api/${moneda}/${fecha}`);
 
-    return data;
+      if (!res.ok) {
+         throw new Error("No se pudo obtener los datos históricos");
+      }
+
+      const data = await res.json();
+
+      return data;
+   } catch (error) {
+      const errorDiv = document.querySelector("#error");
+      errorDiv.textContent = `Error: ${error.message}`;
+   }
 }
 
 async function construyeSelect() {
-    const data = await obtenerDatosDiaActual();
-    
-    const monedas = (Object.keys(data));
-    let html = '<select class="selectorCoin" name="moneda" id="moneda">';
-    for (const codigo_moneda of monedas) {
-        const moneda = data[codigo_moneda];
-        if  (moneda.unidad_medida == 'Pesos') {
-            html+= `<option value="${moneda.codigo}-${moneda.valor}">${moneda.nombre}</option>`;
-        }
-    
-    }
-    html += '</select>';
+   const data = await obtenerDatosDiaActual();
 
-    const selector = document.querySelector('#selector');
-    selector.innerHTML = html;
+   const monedas = Object.keys(data);
+   let html = '<select class="selectorCoin" name="moneda" id="moneda">';
+   for (const codigo_moneda of monedas) {
+      const moneda = data[codigo_moneda];
+      if (moneda.unidad_medida == "Pesos") {
+         html += `<option value="${moneda.codigo}-${moneda.valor}">${moneda.nombre}</option>`;
+      }
+   }
+   html += "</select>";
+
+   const selector = document.querySelector("#selector");
+   selector.innerHTML = html;
 }
 
-function dibujaGrafico(fechas, valores){
-    const ctx = document.querySelector("#miGrafico");
+function dibujaGrafico(fechas, valores) {
+   const ctx = document.querySelector("#miGrafico");
 
-      const data = {
-        labels: fechas,
-        datasets: [{
+   const data = {
+      labels: fechas,
+      datasets: [
+         {
             label: "Historico ultimos 10 dias",
             data: valores,
             fill: false,
             borderColor: "rgb(166, 211, 234)",
             tension: 0.1,
-          },
-        ],
-      };
+         },
+      ],
+   };
 
-      new Chart(ctx, {
-        type: "line",
-        data: data,
+   // Si el gráfico ya existe, actualiza sus datos
+   if (miGrafico) {
+      miGrafico.data = data;
+      miGrafico.update();
+   } else {
+      // Crea un nuevo gráfico si no existe
+      miGrafico = new Chart(ctx, {
+         type: "line",
+         data: data,
       });
+   }
 }
 
-const btnCalcular = document.querySelector('#btnCalcular');
+const btnCalcular = document.querySelector("#btnCalcular");
 
-btnCalcular.addEventListener('click', async function(){
-    const valor = document.querySelector('#valor').value;
-    tasaYMoneda = document.querySelector('#moneda').value.split('-');
+btnCalcular.addEventListener("click", async function () {
+   const valor = document.querySelector("#valor").value;
+   tasaYMoneda = document.querySelector("#moneda").value.split("-");
 
-    const valorConvertido = valor / tasaYMoneda[1];
-    document.querySelector('#resultado').innerHTML = valorConvertido.toFixed(2);
+   const valorConvertido = valor / tasaYMoneda[1];
+   document.querySelector("#resultado").innerHTML = valorConvertido.toFixed(2);
 
-    const codigoMoneda = tasaYMoneda[0];
-    const fechaActual = new Date();
+   const codigoMoneda = tasaYMoneda[0];
+   const fechaActual = new Date();
 
-    let fechas = [];
-    let valores = [];
+   let fechas = [];
+   let valores = [];
 
-    let ultimoValor = 0;
-    for(i = 10; i > 0; i--) {
-        const dia = fechaActual.getDate()-i;
-        const mes = fechaActual.getMonth()+1;
-        const aso = fechaActual.getFullYear();
-        
-        const fechaConsulta = `${dia}-${mes}-${aso}`;
+   let ultimoValor = 0;
+   for (i = 10; i > 0; i--) {
+      const dia = fechaActual.getDate() - i;
+      const mes = fechaActual.getMonth() + 1;
+      const aso = fechaActual.getFullYear();
 
-        const dataHistorica = await obtenerDatosHistoricos(codigoMoneda, fechaConsulta);
+      const fechaConsulta = `${dia}-${mes}-${aso}`;
 
-        fechas.push(fechaConsulta);
-        if (dataHistorica.serie.length > 0) {
+      const dataHistorica = await obtenerDatosHistoricos(codigoMoneda, fechaConsulta);
 
-            valores.push(dataHistorica.serie[0].valor);
-            ultimoValor = dataHistorica.serie[0].valor;
-        } else {
-            valores.push(ultimoValor);
-        }
-    }
-    dibujaGrafico(fechas, valores);
+      fechas.push(fechaConsulta);
+      if (dataHistorica.serie.length > 0) {
+         valores.push(dataHistorica.serie[0].valor);
+         ultimoValor = dataHistorica.serie[0].valor;
+      } else {
+         valores.push(ultimoValor);
+      }
+   }
 
-    
+   dibujaGrafico(fechas, valores);
 });
 
 construyeSelect();
